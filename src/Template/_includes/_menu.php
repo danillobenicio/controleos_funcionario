ab165c3aa14a4b2b00b7253d01f1f73ad40bcfba
Danillo <danillo.benicio@gmail.com>
2024-04-11 23:33:20 -0300

    Ajuste Layout

--- src/Template/_includes/_menu.php
+++ src/Template/_includes/_menu.php
@@ -14,11 +14,11 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="#" class="brand-link">
+  <!--<a href="#" class="brand-link">
     <img src="../../Template/dist/img/service-desk.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
       style="opacity: .8">
     <span class="brand-text font-weight-light">Control OS | Funcionário</span>
-  </a>
+  </a>-->
 
   <!-- Sidebar -->
   <div class="sidebar">
